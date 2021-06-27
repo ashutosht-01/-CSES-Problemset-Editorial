5669c4ce77a16c6f19d510117a5d951712ffce02
Ashutosh <ashutripathi733@gmail.com>
2021-06-27 21:41:57 +0530

    dp section updated

--- Dynamic Programming/Grid Path.cpp
+++ Dynamic Programming/Grid Path.cpp
@@ -26,7 +26,7 @@ using namespace std;
 #define   N               1e5   
 #define   inf             1e18 
 #define   nline           "\n"
-void f_o_x()
+void i_o()
 {
 	fastio;
 	#ifndef ONLINE_JUDGE
@@ -37,7 +37,7 @@ void f_o_x()
 
 int main()
 {
-f_o_x();
+i_o();
 
 		ll n;
 		cin>>n;
